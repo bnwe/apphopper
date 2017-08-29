8ab3b62b1764b1e20f85e0a10a9613df286784cd
Benjamin Wiese <benjamin.wiese@methodpark.de>
2017-08-29 18:58:22 +0200

    Get stdout in text mode

--- appswitch.py
+++ appswitch.py
@@ -2,7 +2,10 @@
 
 import subprocess
 
-completed = subprocess.run(["wmctrl", "-d"], stdout=subprocess.PIPE)
+completed = subprocess.run(
+    ["wmctrl", "-d"], stdout=subprocess.PIPE, universal_newlines=True)
+
+returnCode = completed.returncode  # TODO: Handle errors
 
 print(type(completed.stdout))
 print()
