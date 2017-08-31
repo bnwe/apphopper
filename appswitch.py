6aaca6615ad1082db21151723b115aa54115c3b9
Benjamin Wiese <benjamin.wiese@methodpark.de>
2017-08-31 08:53:34 +0200

    Get number of currently active desktop

--- appswitch.py
+++ appswitch.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
 import subprocess
+import re
 
+# Get desktops from wmctrl
 completed = subprocess.run(
     ["wmctrl", "-d"], stdout=subprocess.PIPE, universal_newlines=True)
 
 returnCode = completed.returncode  # TODO: Handle errors
 
-print(type(completed.stdout))
-print()
-print(completed.stdout)
+desktopStdOut = completed.stdout
+print("Output of wmctrl command:")
+print(desktopStdOut)
+
+# Parse list of desktops for current desktop
+findDesktopRegex = re.compile(r"(\d)\s*\*")
+
+matchedDesktopObjects = findDesktopRegex.search(desktopStdOut)
+numFoundActiveDesktops = len(matchedDesktopObjects.groups())
+# TODO: Handle errors (should be 1)
+activeDesktopAsString = matchedDesktopObjects.group(1)
+print("Active desktop is: " + activeDesktopAsString)
