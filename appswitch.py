3b4a0dfc477d93f45bb11c8436c013c2548839c8
Benjamin Wiese <benjamin.wiese@methodpark.de>
2017-09-01 08:02:57 +0200

    Implement basic app switching

--- appswitch.py
+++ appswitch.py
@@ -1,23 +1,69 @@
 #!/usr/bin/env python3
 
 import subprocess
+import sys
 import re
 
-# Get desktops from wmctrl
+
+def getActiveDesktop():
+    # Get desktops from wmctrl
+    completed = subprocess.run(
+        ["wmctrl", "-d"], stdout=subprocess.PIPE, universal_newlines=True)
+
+    # returnCode = completed.returncode  # TODO: Handle errors
+
+    desktopStdOut = completed.stdout
+    print("Output of wmctrl command:")
+    print(desktopStdOut)
+
+    # Parse list of desktops for current desktop
+    findDesktopRegex = re.compile(r"(\d)\s*\*")
+
+    matchedDesktopObjects = findDesktopRegex.search(desktopStdOut)
+    # numFoundActiveDesktops = len(matchedDesktopObjects.groups())
+    # TODO: Handle errors (should be 1)
+    activeDesktopAsString = matchedDesktopObjects.group(1)
+    print("Active desktop is: " + activeDesktopAsString)
+    return activeDesktopAsString
+
+
+def getRequestedWindowId(windowName, desktopNo):
+    print()
+    # Get windows from wmctrl
+    completed = subprocess.run(
+        ["wmctrl", "-l", "-x"],
+        stdout=subprocess.PIPE,
+        universal_newlines=True)
+    # returnCode = completed.returncode  # TODO: Handle errors
+    windowsStdOut = completed.stdout
+    print(windowsStdOut)
+
+    regexString = r"(0x[\w]+)\s*" + desktopNo + "\s" + windowName
+    print("regex string for finding " + windowName + ": " + regexString)
+    findWindowRegex = re.compile(regexString, re.IGNORECASE)
+
+    matchedWindowObjects = findWindowRegex.search(windowsStdOut)
+    if matchedWindowObjects is None:
+        print("ERROR: The window " + windowName +
+              " could not be uniquely identified on desktop " + desktopNo)
+        return
+    else:
+        windowIdAsString = matchedWindowObjects.group(1)
+        print("window id:" + windowIdAsString)
+        return windowIdAsString
+
+
+desktopNo = getActiveDesktop()
+# TODO: check length of sys.argv
+requestedWindowName = sys.argv[1]
+winID = getRequestedWindowId(requestedWindowName, desktopNo)
+
+print("Now switching to winid " + winID)
 completed = subprocess.run(
-    ["wmctrl", "-d"], stdout=subprocess.PIPE, universal_newlines=True)
+        ["wmctrl", "-i", "-a", winID],
+        stdout=subprocess.PIPE,
+        universal_newlines=True)
+# returnCode = completed.returncode  # TODO: Handle errors
+windowsStdOut = completed.stdout
+print(windowsStdOut)
 
-returnCode = completed.returncode  # TODO: Handle errors
-
-desktopStdOut = completed.stdout
-print("Output of wmctrl command:")
-print(desktopStdOut)
-
-# Parse list of desktops for current desktop
-findDesktopRegex = re.compile(r"(\d)\s*\*")
-
-matchedDesktopObjects = findDesktopRegex.search(desktopStdOut)
-numFoundActiveDesktops = len(matchedDesktopObjects.groups())
-# TODO: Handle errors (should be 1)
-activeDesktopAsString = matchedDesktopObjects.group(1)
-print("Active desktop is: " + activeDesktopAsString)
